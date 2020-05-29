07f62829293d49e4d96789283a6f352f48093042
Neil Jackson <neil@rattnow.com>
2020-05-28 20:39:45 -0700

    fixed crlf line ending problem that was throwing sh for a loop

--- temperature_monitor/temperature_monitor.sh
+++ temperature_monitor/temperature_monitor.sh
@@ -2,13 +2,13 @@
 #by neil
 
 #here is a daemon-like shell script that polls the kaiten repl interface every few seconds, and also timestamps and dumps to a log file each of the lines outputted by the kaiten repl.
-(
+
 # echo $$
 pipeIntoRepl="/tmp/neiltemp1";
 pipeOutOfRepl="/tmp/neiltemp2";
 logFile="/tmp/neil_log_1";
 timestampFormat="%F_%T";
-
+#(
 #clean up procedure:
 # trap "echo ; echo cleaning up...; echo \"logging the fact that we are ceasing logging ...\"; echo \"ceasing logging at \$(date +${timestampFormat})\" >> ${pipeOutOfRepl}; echo \"waiting 1 second to allow the logging apparatus to log the cessation of logging before we kill the logging apparatus ...\"; sleep 1; echo killing processes \$(jobs -p) ...; kill \$(jobs -p); echo deleting pipes ${pipeIntoRepl}  ${pipeOutOfRepl} ...; rm ${pipeIntoRepl}  ${pipeOutOfRepl} 1> /dev/null 2>&1;  echo \"invoking \\\"jobs\\\" one last time...\";  jobs 1> /dev/null 2>&1; " SIGINT;
 # trap "echo ; echo cleaning up...; echo \"making sure the output pipe is open so that our logging of the cessation of logging will wait until the loggin apparatus will receive the message before proceeding to kill the logging apparatus. ...\"; exec 9<> ${pipeOutOfRepl}; echo \"logging the fact that we are ceasing logging ...\"; echo \"ceasing logging at \$(date +${timestampFormat})\" > ${pipeOutOfRepl}; echo killing processes \$(jobs -p) ...; kill \$(jobs -p); echo deleting pipes ${pipeIntoRepl}  ${pipeOutOfRepl} ...; rm ${pipeIntoRepl}  ${pipeOutOfRepl} 1> /dev/null 2>&1;  echo \"invoking \\\"jobs\\\" one last time...\";  jobs 1> /dev/null 2>&1; " SIGINT;
@@ -38,7 +38,7 @@ watch -n 2 "echo -e \"[self._machine_manager._pymach.get_current_command_index()
 
 #at this point, all of our asynchronous tasks are cranking away and we can sit back and relax
 
-echo waiting for ${logFile} to exist...;
+# echo waiting for ${logFile} to exist...;
 # wait for the log file to exist, so as  not to cause the tail command, below, to throw an error when it tries to read a non-existent file.
 while [ ! -f ${logFile} ]; do echo log file does not yet exist.  waiting...; sleep 1; done;
 
@@ -51,6 +51,6 @@ tail -f ${logFile} ;
 #if we did not do something explicit here to wait, we would fall out the bottom of the subshell and the 
 # cleanup up command that we registered with trap, above, would fire, stopping the logging process.
 #wait for 
-wait $(jobs -p)
-)
+wait $(jobs -p);
+#)
 
